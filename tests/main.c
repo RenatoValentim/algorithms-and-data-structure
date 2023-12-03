dec8b5e2497b31925d24190e8c334be6e74492cb
rvjs <renato.valentim@tempest.com.br>
2023-12-03 14:46:49 -0300

    refactor: organize tests main

--- tests/main.c
+++ tests/main.c
@@ -11,21 +11,26 @@ void tearDown(void) {}
 int main(void) {
   UNITY_BEGIN();
 
+  // structured binary search
   RUN_TEST(shouldReturnCorrectPositionAndValue);
   RUN_TEST(shouldReturnNegativePositionAndValueIfValueNotFound);
   RUN_TEST(shouldReturnNegativePositionAndValueIfArrayEmpty);
   RUN_TEST(shouldReturnNegativePositionAndValueIfArrayNULL);
   RUN_TEST(shouldReturnNegativePositionAndValueIfArraySizeEqualZero);
   RUN_TEST(shouldReturnNegativePositionAndValueIfArraySizeNeative);
+
+  // recursive sum
   RUN_TEST(shouldSumAllValuesAndReturnTheTotal);
   RUN_TEST(shouldReturnImmediatelyTheValueIfArrayHasOnlyOneValue);
   RUN_TEST(shouldReturnZeroIfTheArrayIsEmpty);
 
+  // recursive get get array size
   RUN_TEST(shouldReturnTotalItemsArrWithSequentialValues);
   RUN_TEST(shouldReturnTotalItemsArrWithoutSequentialValues);
   RUN_TEST(shouldReturnZeroIfEmptyArray);
   RUN_TEST(shouldReturnNegativeNumberIfIndexLessThenZero);
 
+  // recursive binary search
   RUN_TEST(shouldReturnCorrectPositionAndValue_Recursive);
   RUN_TEST(shouldReturnNegativePositionAndValueIfValueNotFound_Recursive);
   RUN_TEST(shouldReturnNegativePositionAndValueIfArrayEmpty_Recursive);
