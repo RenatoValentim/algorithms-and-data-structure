1f986fbb7af30b710bdfd077f786c72467a67579
RenatoValentim <rvrenato28@gmail.com>
2023-12-25 05:12:57 -0300

    feat(Go/recursive/binary_search): return a negative index if arr is nil

---
+++ Go/internal/recursive/binary_search.go
@@ -0,0 +1,8 @@
+package recursive
+
+func BinarySearch(arr []int, val int) int {
+	if arr == nil {
+		return -1
+	}
+	return -1
+}
--- Go/internal/recursive/binary_search_test.go
+++ Go/internal/recursive/binary_search_test.go
@@ -1,6 +1,7 @@
 package recursive_test
 
 import (
+	"algorithms_data_structures/internal/recursive"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,7 +9,9 @@ import (
 
 func TestBinarySearch(t *testing.T) {
 	assert := assert.New(t)
-	t.Run(`test the test`, func(t *testing.T) {
-		assert.Equal(1, 1)
+	t.Run(`Should return a negative index if arr is nil`, func(t *testing.T) {
+		output := recursive.BinarySearch(nil, 3)
+
+		assert.Equal(-1, output)
 	})
 }
