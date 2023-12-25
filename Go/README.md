64adac819f90c4982dfd42909698a13f793582c9
RenatoValentim <rvrenato28@gmail.com>
2023-12-24 23:20:15 -0300

    refactor: add Golang to repo

---
+++ Go/README.md
@@ -0,0 +1,8 @@
+# Algorithms and Data Structure
+
+Studies of bases such as algorithms data structures...
+
+- Run all tests without docker
+```BASH
+make
+```
---
+++ Go/internal/recursive/binary_search_test.go
@@ -0,0 +1,14 @@
+package recursive_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBinarySearch(t *testing.T) {
+	assert := assert.New(t)
+	t.Run(`test the test`, func(t *testing.T) {
+		assert.Equal(1, 1)
+	})
+}
