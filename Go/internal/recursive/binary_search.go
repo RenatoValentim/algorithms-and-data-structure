589f5b5b8692edee8ecd39d9615acad8802ad3cf
RenatoValentim <rvrenato28@gmail.com>
2023-12-25 05:28:36 -0300

    feat(Go/recursive/binary_search): return the index for the searched value when the guess is equals for the value

--- Go/internal/recursive/binary_search.go
+++ Go/internal/recursive/binary_search.go
@@ -4,5 +4,12 @@ func BinarySearch(arr []int, val int) int {
 	if arr == nil || len(arr) == 0 {
 		return -1
 	}
+	highestIndex := 0
+	lowestIndex := len(arr) - 1
+	middleIndex := (highestIndex + lowestIndex) / 2
+	guess := arr[middleIndex]
+	if guess == val {
+		return middleIndex
+	}
 	return -1
 }
