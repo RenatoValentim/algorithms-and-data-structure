08adf8f7814437b26e97c169b1391682c4167b4d
RenatoValentim <rvrenato28@gmail.com>
2024-01-01 23:53:37 -0300

    fix(Go/recursive/binary_search): fix variable name

--- Go/internal/recursive/binary_search.go
+++ Go/internal/recursive/binary_search.go
@@ -4,8 +4,8 @@ func BinarySearch(arr []int, val int) int {
 	if arr == nil || len(arr) == 0 {
 		return -1
 	}
-	highestIndex := 0
-	lowestIndex := len(arr) - 1
+	lowestIndex := 0
+	highestIndex := len(arr) - 1
 	middleIndex := (highestIndex + lowestIndex) / 2
 	guess := arr[middleIndex]
 	if guess == val {
